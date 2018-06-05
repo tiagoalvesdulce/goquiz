0dc958978a9b243eb73cec850d8a9a1fd95ef1b0
Tiago Alves Dulce <tiagoalvesdulce@outlook.com>
2018-06-05 15:22:42 -0300

    improve quiz(add timer and improve csv reading)

--- main.go
+++ main.go
@@ -5,46 +5,58 @@ import (
 	"encoding/csv"
 	"flag"
 	"fmt"
-	"io"
 	"log"
 	"os"
 	"strings"
+	"time"
 )
+
+func countTime(limit int, done chan<- bool) {
+	time.Sleep(time.Duration(limit) * time.Second)
+	done <- true
+}
 
 func main() {
 	quizFilename := flag.String("quizFilename", "problems.csv", "The name of the file where the quiz is")
+	limit := flag.Int("limit", 30, "The time limit for a quiz in seconds")
 	flag.Parse()
-	fmt.Println("Loading quiz: ", *quizFilename)
+
+	var rightAnswers int
+	var done = make(chan bool, 1)
+
+	fmt.Println("... Loading quiz: ", *quizFilename)
 	quizFile, err := os.Open(*quizFilename)
 	if err != nil {
 		log.Fatalf("Error openning quiz file. Err: %s", err)
 	}
 	reader := csv.NewReader(bufio.NewReader(quizFile))
-	var totalQuestions, rightAnswers int
-	for {
-		line, err := reader.Read()
-		if err == io.EOF {
-			break
-		}
-		if err != nil {
-			log.Fatalf("Could not read. Err: %s", err)
-		}
-		question := line[0]
-		answer := strings.Trim(strings.ToLower(line[1]), " ")
 
-		var userAnswer string
-		fmt.Printf("%s = ", question)
-		_, err = fmt.Scan(&userAnswer)
-		if err != nil {
-			log.Fatalf("Error getting answer from user. Err: %s", err)
-		}
-
-		userAnswer = strings.Trim(strings.ToLower(userAnswer), " ")
-
-		if userAnswer == answer {
-			rightAnswers++
-		}
-		totalQuestions++
+	go countTime(*limit, done)
+	lines, err := reader.ReadAll()
+	if err != nil {
+		log.Fatalf("Could not read. Err: %s", err)
 	}
-	fmt.Printf("You got %d/%d\n", rightAnswers, totalQuestions)
+
+	go func() {
+		for i, problem := range lines {
+			question := problem[0]
+			answer := strings.Trim(strings.ToLower(problem[1]), " ")
+			var userAnswer string
+
+			fmt.Printf("Problem #%d: %s = ", i+1, question)
+
+			_, err = fmt.Scan(&userAnswer)
+			if err != nil {
+				log.Fatalf("Error getting answer from user. Err: %s", err)
+			}
+
+			userAnswer = strings.Trim(strings.ToLower(userAnswer), " ")
+
+			if userAnswer == answer {
+				rightAnswers++
+			}
+		}
+	}()
+	<-done
+	fmt.Printf("\nYou got %d/%d\n", rightAnswers, len(lines))
 }
